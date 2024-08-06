5262244e162c570ddb906f9e06556a7e67ace629
praveen <mspraveenkumar77@gmail.com>
2024-08-06 15:04:20 +0000

    Updates

--- test.c
+++ test.c
@@ -1,13 +1,48 @@
-//gcc -g -o factorial factorial.c
 #include <stdio.h>
-// for loop to print 1 to 10
+int k = 30;
+
+int foo(){
+    static int q = 200;
+    printf("[*] Address of q: %p\n", &q);
+    printf("[*] Value of q: %d\n", q);
+}
 
 int main() {
-    int i;
-    // printf("hello !");
-    for (i = 1; i <= 10; i++) {
-        // printf("%d\n", i);
-        printf("John");
-    }
+    static int j = 20;
+    static int l = 50;
+    printf("[*] Address of k: %p\n", &k);
+    printf("[*] Address of j: %p\n", &j); 
+    printf("[*] Address of l: %p\n", &l);
+
+    int *m = &j - 0x2; // illega access;
+    printf("[*] Address of M: %p\n", m);
+    printf("[*] Value of k: %d\n", k);
+    printf("[*] Value of j: %d\n", j);
+    printf("[*] Value of l: %d\n", l);
+
+    *m = 10;
+    printf("Changing the value of K to 10 Via Pointer M\n");
+    printf("[*] Value of k: %d\n", k);
+    printf("[*] Value of j: %d\n", j);
+    printf("[*] Value of l: %d\n", l);
+
+    int *u = &j + 0x1; // illegal access
+    printf("[*] Address of U: %p\n", u);
+    printf("[*] Value of k: %d\n", k);
+    printf("[*] Value of j: %d\n", j);
+    printf("[*] Value of l: %d\n", l);
+
+    *u = 200;
+    printf("Changing the value of L to 200 Via Pointer U\n");
+    printf("[*] Value of k: %d\n", k);
+    printf("[*] Value of j: %d\n", j);
+    printf("[*] Value of l: %d\n", l);
+
+    foo();
+    int *p = &l - 0x2; // illegal access, refers static variable from other function !!
+    printf("[*] Address of P [main]: %p\n", p);
+    *p = 300;
+    foo();
     return 0;
 }
+ 
